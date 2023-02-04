1db98ba57b34b4cfc5bc06bbe21bd67a11c079cf
CalvinM22 <115098757+CalvinM22@users.noreply.github.com>
2023-02-04 17:17:00 +0000

    half done

---
+++ src_GlobalGameJam23/Assets/Scripts/Player/SpritesSwap.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpritesSwap : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = newSprite;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeSprite(newSprite);
+        }
+    }
+    
+}
