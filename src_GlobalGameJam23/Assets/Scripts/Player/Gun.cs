0e1daa531f0136e64ddb17a06901755490816513
Tobyshull <112322872+Tobys577@users.noreply.github.com>
2023-02-04 18:30:40 +0000

    Revert "Merge pull request #6 from Tobys577/Guns-blub-blub-"
    This reverts commit 9d0c5c521066f2b841e56975f38e994af36698e8, reversing
    changes made to 5e02929a69dbcc09156f3ccf35997db4889a2f59.

--- src_GlobalGameJam23/Assets/Scripts/Player/Gun.cs
+++ src_GlobalGameJam23/Assets/Scripts/Player/Gun.cs
@@ -1,16 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using TMPro.Examples;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
     [SerializeField] public Transform gunPoint;
     [SerializeField] public Transform bulletTrail;
-    [SerializeField] public float range = 100f;
-    public float variability = 1f;
+    [SerializeField] public float range = 10f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -29,20 +26,9 @@ public class Gun : MonoBehaviour
 
     void Shoot()
     {
-        bool moving = false;
-        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        {
-            moving = true;
-        }
-
-        Vector3 originalEulerAngle = transform.eulerAngles;
-        transform.eulerAngles = new Vector3(0, 0, moving ? Random.Range(transform.eulerAngles.z - 10, transform.eulerAngles.z + 10) : transform.eulerAngles.z);
-
-        RaycastHit2D hit = Physics2D.Raycast(gunPoint.position, transform.up ,range);
+        RaycastHit2D hit = Physics2D.Raycast(gunPoint.position, transform.up, range);
         Transform trail = Instantiate(bulletTrail, gunPoint.position, transform.rotation);
         BulletTrailScript trailScript = trail.GetComponent<BulletTrailScript>();
-
-        
 
         if(hit.collider != null)
         {
@@ -54,7 +40,6 @@ public class Gun : MonoBehaviour
             Vector3 endPos = gunPoint.position + transform.up * range;
             trailScript.SetTargetPosition(endPos);
         }
-        transform.eulerAngles = originalEulerAngle;
 
     }
 }
