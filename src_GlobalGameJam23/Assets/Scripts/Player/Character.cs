4a19cf060af9434c79a289e242c8c8c9e03b41d5
Tobyshull <112322872+Tobys577@users.noreply.github.com>
2023-02-04 18:29:30 +0000

    Revert "Merge branch 'main' into Guns-blub-blub-"
    This reverts commit 33809d866f537a7534d1abaad20072f8baa53d81, reversing
    changes made to f61587a3bdf27cdd83e6c1b1c9af3c69897d38c1.

---
+++ src_GlobalGameJam23/Assets/Scripts/Player/Character.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Character
+{
+    public Sprite icon;
+    public Sprite playerSpr;
+
+    public string characterName;
+}
