0bb293f69a20771c928d0604faa139b8b1e40164
Tobyshull <112322872+Tobys577@users.noreply.github.com>
2023-02-05 07:50:09 +0000

    Manual Merge with multiplayer-shoot-stuff

--- src_GlobalGameJam23/Assets/Scripts/Player/Gun.cs
+++ src_GlobalGameJam23/Assets/Scripts/Player/Gun.cs
@@ -1,3 +1,4 @@
+using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -5,33 +6,39 @@ using TMPro.Examples;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
+public class Gun : NetworkBehaviour
 {
     [SerializeField] public Transform gunPoint;
     [SerializeField] public Transform bulletTrail;
     [SerializeField] public float range = 100f;
     public float variability = 1f;
-    bool attacking = true;
+
+    [Networked] public bool attacking { set; get; }
+
+    private NetworkObject networkObject;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        networkObject = GetComponent<NetworkObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (networkObject.HasInputAuthority)
         {
-            Shoot();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
         }
     }
 
     void Shoot()
     {
         bool moving = false;
-        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             moving = true;
         }
@@ -39,21 +46,17 @@ public class Gun : MonoBehaviour
         Vector3 originalEulerAngle = transform.eulerAngles;
         transform.eulerAngles = new Vector3(0, 0, moving ? Random.Range(transform.eulerAngles.z - 10, transform.eulerAngles.z + 10) : transform.eulerAngles.z);
 
-        RaycastHit2D hit = Physics2D.Raycast(gunPoint.position, transform.up ,range);
+        RaycastHit2D hit = Physics2D.Raycast(gunPoint.position, transform.up, range);
         Transform trail = Instantiate(bulletTrail, gunPoint.position, transform.rotation);
         BulletTrailScript trailScript = trail.GetComponent<BulletTrailScript>();
 
-        
 
-        if(hit.collider != null)
+        if (hit.collider != null)
         {
-            Debug.Log("hit something");
-            Debug.Log(hit.collider.name);
             trailScript.SetTargetPosition(hit.point);
-            if(hit.collider.gameObject.tag == "Player" && attacking)
+            if (hit.collider.gameObject.transform.parent.tag == "Player" && hit.collider.gameObject.GetComponentInParent<Gun>().attacking != attacking)
             {
                 hit.collider.gameObject.GetComponentInParent<PlayerLife>().UpdateHealth(30);
-                Debug.Log("damaged player");
             }
         }
         else
--- src_GlobalGameJam23/Assets/Scripts/Player/PlayerLife.cs
+++ src_GlobalGameJam23/Assets/Scripts/Player/PlayerLife.cs
@@ -1,26 +1,55 @@
+using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLife : MonoBehaviour
+public class PlayerLife : NetworkBehaviour
 {
-    int health = 100;
-    void Start()
+    public int health = 100;
+
+    NetworkObject netObj;
+    GameObject diedCanvas;
+    private void Start()
     {
-        
+        netObj = GetComponent<NetworkObject>();
+        if (netObj.HasStateAuthority)
+        {
+            diedCanvas = GameObject.Find("Died");
+            diedCanvas.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(health < 0)
+        if (netObj.HasStateAuthority)
         {
-            Destroy(gameObject);
+            if (health < 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
     public void UpdateHealth(int damage)
     {
+        Debug.Log("Dealth Damage " + damage);
         health -= damage;
+        RPC_UpdateHealth(health);
+    }
+
+    [Rpc]
+    public void RPC_UpdateHealth(int newHealth)
+    {
+        health = newHealth;
+    }
+
+    private void OnDestroy()
+    {
+        if (diedCanvas == null)
+            return;
+
+        diedCanvas.SetActive(true);
+        Runner.Despawn(GetComponent<NetworkObject>());
     }
 }
